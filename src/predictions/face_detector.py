15002c983f8b09118b933e1ba42f49d7e3f36311
sqoshi <!piter68>
2021-09-16 21:14:46 +0200

    embeddings

--- src/predictions/face_detector.py
+++ src/predictions/face_detector.py
@@ -1,6 +1,7 @@
 import cv2
 import dlib
 import pandas as pd
+import warnings
 from _dlib_pybind11 import get_frontal_face_detector
 from imutils import resize
 
@@ -17,29 +18,58 @@ def rect_to_bb(rect: dlib.rectangle):
 
 
 def draw_sample(image, rect_list, crop=True, delay=1000):
+    def _display_img(img, dly):
+        cv2.imshow("Output Sample", img)
+        cv2.waitKey(dly)
+
     for rect in rect_list:
         x, y, w, h = rect_to_bb(rect)
         cv2.rectangle(image, (x, y), (x + w, y + h), (30, 144, 255), 2)
         cv2.putText(image, "Face", (x - 10, y - 10), cv2.FONT_ITALIC, 0.5, (90, 158, 233), 2)
-
         if crop:
             image = image[rect.top():rect.bottom(), rect.left():rect.right()]
             image = cv2.resize(image, (96, 96), interpolation=cv2.INTER_AREA)
-            cv2.imshow("Output Sample", image)
-            cv2.waitKey(delay)
+            _display_img(image, delay)
+
     if not crop:
-        cv2.imshow("Output Sample", image)
-        cv2.waitKey(delay)
+        _display_img(image, delay)
+
+
+def warn_detections(face_detections):
+    if len(face_detections) > 1:
+        warnings.warn(f"Detected {len(face_detections)} faces on image.")
+    elif len(face_detections) == 0:
+        warnings.warn("Could not detect face on image.")
+
+
+def crop(image, rect):
+    return image.obj[rect.top():rect.bottom(), rect.left():rect.right()]
 
 
 class FaceDetector:
-    def __init__(self, dataset_df: pd.DataFrame) -> None:
+    def __init__(self, dataset_df: pd.DataFrame, embedder_fp="../models/nn4.small2.v1.t7") -> None:
         self.dataset_df = dataset_df
         self._detector = get_frontal_face_detector()
+        self._embedder = cv2.dnn.readNetFromTorch(embedder_fp)
+        self.target_size = (96, 96)
+
+    def vector(self, face_crop):
+        face_blob = cv2.dnn.blobFromImage(face_crop, 1.0 / 255, self.target_size, (0, 0, 0), swapRB=True, crop=False)
+        self._embedder.setInput(face_blob)
+        vec = self._embedder.forward()
+        print(f"Face vector shape is {vec.shape}.")
+        return vec.flatten()
 
     def detect(self):
         for index, row in self.dataset_df.iterrows():
             img = Image(row['filename'], row['identity'])
             print(img)
             face_rectangles = self._detector(img.obj, 1)
-            draw_sample(img.obj, face_rectangles, True)
+            warn_detections(face_rectangles)
+            # draw_sample(img.obj, face_rectangles, True)
+            # todo: adjust rectangle
+            rect = face_rectangles.pop()
+            face_crop = crop(img.obj, rect)
+            # img.set_face(face_crop)
+            embeddings_vec = self.vector(face_crop)
+            print(embeddings_vec)
--- src/predictions/image.py
+++ src/predictions/image.py
@@ -1,13 +1,25 @@
 from typing import Optional
 
 import cv2
+import imutils
 
 
 class Image:
-    def __init__(self, path: str, identity: Optional[str] = None) -> None:
+    def __init__(self, path: str, identity: Optional[str] = None, size=(96, 96)) -> None:
         self.obj = cv2.imread(path)
         self.identity = identity
         self.path = path
+        self._face = None
+        self.target_size = size
+
+    def get_face(self):
+        return self._face
+
+    def set_face(self, face):
+        if self._face is None:
+            # height, width, _ = face.shape
+            # if (height,width) != self.target_size:
+            self._face = imutils.resize(face, *self.target_size)
 
     def __repr__(self):
         return f"{self.__class__.__name__}(identity={self.identity!r}, path={self.path!r}, size={self.obj.shape!r})"
