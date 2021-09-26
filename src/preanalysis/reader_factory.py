0f15686c351ad4975d0bfaecbd08d5fe97f9f632
sqoshi <!piter68>
2021-09-26 19:57:41 +0200

    reader factory added

---
+++ src/preanalysis/reader_factory.py
@@ -0,0 +1,94 @@
+import logging
+import os
+from enum import Enum
+from typing import List, Callable
+
+import numpy as np
+import pandas as pd
+from imutils.paths import list_images
+from numpy import isnan
+
+from preanalysis.defs import DatasetConfig
+
+logger = logging.getLogger(__name__)
+
+
+class Strategy(Enum):
+    """Represents structure of images kept under dataset directory path."""
+    grouped = "grouped"
+    mixed = "mixed"  # requires identity file
+
+
+def _limit(dataset_df: pd.DataFrame, n: int) -> pd.DataFrame:
+    """Limits identities images to `n` images per identity.."""
+    statistics = dataset_df["identity"].value_counts()[(dataset_df["identity"].value_counts() >= n)]
+    identities = statistics.keys().tolist()
+    if isnan(statistics.max()) or statistics.max() < n:
+        raise ValueError(f"There is not enough ({n}) images for any identity. Max is {statistics.max()}.")
+    return dataset_df.loc[dataset_df["identity"].isin(identities)].copy()
+
+
+def auto_strategy(dc: DatasetConfig) -> Strategy:
+    """Selects strategy depending on identities file existence."""
+    return Strategy.grouped if dc.identities_fp is None else Strategy.mixed
+
+
+class ReaderFactory:
+    """Accordingly to input dataset config reading dataset with appropriate strategy."""
+
+    def __init__(self, columns: List[str] = ("filename", "identity")) -> None:
+        self.columns = columns
+
+    def read(self, dc: DatasetConfig, n: int, strategy: Strategy = None) -> pd.DataFrame:
+        """Read dataset with n images per identity with special strategy."""
+        strategy = auto_strategy(dc) if strategy is None else strategy
+        reader = self._get_reader(strategy)
+        return reader(dc, n)
+
+    def _get_reader(self, strategy: Strategy = None) -> Callable:
+        """Selects reading methodology by strategy."""
+        if strategy == Strategy.grouped:
+            return self._read_grouped
+        if strategy == Strategy.mixed:
+            return self._read_mixed
+        raise ValueError(strategy)
+
+    def _read_grouped(self, dc: DatasetConfig, n: int = 1) -> pd.DataFrame:
+        """Reading n images per identity in identity-grouped structure.
+
+        Example structure:
+            dataset
+            ├── andrzej_duda
+            │         ├── 0.png
+            │         └── 1.png
+            │
+            ├── andrzej_stefaniak
+            │         ├── 0.png
+            │         └── 1.png
+            │
+            ...
+
+        """
+        logger.info("Reading dataset grouped by identity images in subdirectory.")
+        images = list(list_images(dc.directory_fp))
+        identities = [path.split(os.sep)[-2] for path in images]
+        dataset_df = pd.DataFrame(list(zip(images, identities)), columns=self.columns)
+        dataset_df = _limit(dataset_df, n)
+        return dataset_df
+
+    def _read_mixed(self, dc: DatasetConfig, n: int = 1) -> pd.DataFrame:
+        """Reading n images per identity in mixed images d.
+
+        Example structure:
+            dataset
+            ├── attributes.csv (must contain columns identity, filename)
+            ├── 0.png
+            ├── 1.png
+            ├── 2.png
+            ...
+        """
+        logger.info("Reading dataset mixed with identities in .csv file.")
+        attrs_df = pd.read_csv(dc.identities_fp, sep=" ", index_col=False, names=self.columns)
+        dataset_df = _limit(attrs_df, n)
+        dataset_df["filename"] = dataset_df["filename"].apply(lambda x: os.path.join(dc.directory_fp, str(x)))
+        return dataset_df
--- src/runner.py
+++ src/runner.py
@@ -1,7 +1,7 @@
 import logging
 import coloredlogs
 
-from preanalysis.analyzer import DatasetReader
+from preanalysis.reader import DatasetReader
 from preanalysis.defs import DatasetConfig
 from predictions.extractor import FaceExtractor
 from predictions.face_recognizer import FaceRecognizer
@@ -17,14 +17,13 @@ coloredlogs.install(
 if __name__ == '__main__':
     logger.info("Program started.")
     dfr = DatasetReader(
-        # DatasetConfig("/home/piotr/Documents/bsc-thesis/datasets/original", None),
-        DatasetConfig("/home/piotr/Documents/bsc-thesis/datasets/celeba/img_align_celeba",
-                      "/home/piotr/Documents/bsc-thesis/datasets/celeba/identity_CelebA.txt")
+        DatasetConfig("/home/piotr/Documents/bsc-thesis/datasets/original", None),
+        # DatasetConfig("/home/piotr/Documents/bsc-thesis/datasets/celeba/img_align_celeba",
+        #               "/home/piotr/Documents/bsc-thesis/datasets/celeba/identity_CelebA.txt")
     )
 
-    dataset = dfr.read(25, equalize=True)
+    dataset = dfr.read()
     train_set, test_set = dfr.split_dataset(dataset)
-    print(f"{len(dataset)} == {len(train_set)} + {len(test_set)}")
     fd = FaceExtractor(train_set)
     embs = fd.extract()
     fd.save()
